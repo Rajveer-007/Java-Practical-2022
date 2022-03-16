7d6b4abcb971ed384d7e0fb5ee4cced438c54d35
Rajveer Mahida <84197480+Rajveer-007@users.noreply.github.com>
2022-03-16 12:31:37 +0530

    Update time.java

--- Practical 3/time.java
+++ Practical 3/time.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class time {
     int sec;
     int min;
@@ -15,16 +17,12 @@ public class time {
         int sumMin= t1.min + t2.min;
         int sumSec = t1.sec + t2.sec;
 
-        System.out.println("Sum of two Time Object is " + sumHour + ":" + sumMin + ":" + ":" + sumSec);
+        System.out.println("Sum of two Time Object is " + sumHour + ":" + sumMin + ":"  + sumSec);
     }
     public static void main(String[] args) {
-       // Here insted of direct passing the value of Hour,min and second you also can take input from user
-        time t1 = new time(10,30,12);
-        time t2 = new time(5,15,12);
-        sumTime(t1,t2);
-        
-        /* 
-        Scanner in = new Scanner(System.in);
+
+      /* => Methode 1
+      Scanner in = new Scanner(System.in);
         
         System.out.println("Enter Time T1 members values");
         int ht1 = in.nextInt();
@@ -44,8 +42,12 @@ public class time {
         System.out.println("Time Object T1 is " + ht1 + ":" + mt1 +  ":" + st1);
         System.out.println("Time Object T2 is " + ht2 + ":" + mt2 +  ":" + st2);
         sumTime(t1,t2);
-        
-        */
+     */ 
+    // ==>Methode 2
+        time t1 = new time(10,30,12);
+        time t2 = new time(5,15,12);
+        sumTime(t1,t2);
+       
     }
 
 }
